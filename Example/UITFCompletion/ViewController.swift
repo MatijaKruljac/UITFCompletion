735081e50825fd1574d3e741fbf2a12bf78e6c84
Matija Kruljac <kruljac.matija@gmail.com>
2017-05-12 09:41:03 +0200

    Added possibility to update tag character and collection in Example.

--- Example/UITFCompletion/ViewController.swift
+++ Example/UITFCompletion/ViewController.swift
@@ -9,40 +9,50 @@
 import UIKit
 import UITFCompletion
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var userInputTextField: UITextField!
     @IBOutlet weak var tagsTextView: UITextView!
+    @IBOutlet weak var tagCharacterTextField: UITextField!
     
     private var uitfCompletionHandler: UITFCompletionHandler!
     
     private var data = ["shark", "lion", "giraffe", "elephant", "orangutan"]
+    private var tagCharacter = "@"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "UITFCompletion Example"
-        setupBackgroundColor()
+    
+        setupTextUIElementes()
+        setupBackgroundColors()
         setupUITFCompletionHandler()
         displayTags()
     }
     
-    private func setupBackgroundColor() {
+    private func setupTextUIElementes() {
+        tagsTextView.delegate = self
+        tagCharacterTextField.delegate = self
+        tagCharacterTextField.text = tagCharacter
+    }
+    
+    private func setupBackgroundColors() {
         view.backgroundColor = UIColor(red: 1, green: 0.9098, blue: 0.9098, alpha: 1.0)
+        tagsTextView.backgroundColor = .white
     }
     
     private func setupUITFCompletionHandler() {
         guard let parentView = userInputTextField.superview else { return }
         
-        // In init you can also pass collection with default tagCharacter "@" or not
+        // in init collection can be passed or not - if not default tagCharacter is "@"
         uitfCompletionHandler = UITFCompletionHandler.init(with: userInputTextField, withParentView: parentView)
         
-        // TagCharacter is passed (it can be any character) - default is "@"
-        uitfCompletionHandler.setup(collection: data, withTagCharacter: "#")
+        // when setting up collection desired tagCharacter can be passed - default is "@"
+        uitfCompletionHandler.setup(collection: data, withTagCharacter: tagCharacter)
     }
     
     private func displayTags() {
         let mutableString = NSMutableString()
-        mutableString.append("TAGS: ")
         for index in 0...data.count-1 {
             if index < data.count-1 {
                 mutableString.append(data[index] + ", ")
@@ -51,6 +61,27 @@ class ViewController: UIViewController {
             }
         }
         tagsTextView.text = mutableString as String
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        updateCollection()
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        updateCollection()
+    }
+    
+    private func updateCollection() {
+        data.removeAll()
+        data = tagsTextView.text.components(separatedBy: ",").map{
+            $0.trimmingCharacters(in: .whitespaces)
+        }
+        if let tagCharacter = tagCharacterTextField.text {
+            self.tagCharacter = tagCharacter
+        }
+        
+        // update collection
+        uitfCompletionHandler.setup(collection: data, withTagCharacter: tagCharacter)
     }
 }
 
